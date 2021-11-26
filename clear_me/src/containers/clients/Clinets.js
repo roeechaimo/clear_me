3bc7ef109ee69594ca2619297a9d5dd07c868b5f
roeec-inmanage <roeec@restartit.co.il>
2021-11-26 16:59:30 +0200

    init routes

---
+++ clear_me/src/containers/clients/Clinets.js
@@ -0,0 +1,7 @@
+export default function Clients() {
+  return (
+    <main>
+      <h2>Clients</h2>
+    </main>
+  );
+}
---
+++ clear_me/src/containers/map/Map.js
@@ -0,0 +1,7 @@
+export default function Map() {
+  return (
+    <main>
+      <h2>Map</h2>
+    </main>
+  );
+}
