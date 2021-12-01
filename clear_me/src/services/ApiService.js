048ab87872d27ab6d382831e031f826d5aa5fea7
roeec-inmanage <roeec@restartit.co.il>
2021-12-01 15:41:17 +0200

    add firbase config

--- clear_me/src/services/ApiService.js
+++ clear_me/src/services/ApiService.js
@@ -1,7 +1,7 @@
 export default class ApiService {
   #baseUrl = 'https://61a689cf8395690017be9324.mockapi.io/api/v1/';
   #zipCodeApiKey = 'PevE06UaIpAoGaA65tO9z0F2SdW8Vwk1';
-  #zipCodesUrl = `http://www.mapquestapi.com/geocoding/v1/batch?key=${this.#zipCodeApiKey}`;
+  #zipCodesUrl = `https://www.mapquestapi.com/geocoding/v1/batch?key=${this.#zipCodeApiKey}`;
 
   getOrganizations(callback) {
     fetch(`${this.#baseUrl}organization`)
