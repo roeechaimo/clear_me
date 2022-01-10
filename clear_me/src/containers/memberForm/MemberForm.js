dac3eea7a7eabacacd9e83d630eba28cb3a42c7f
roeec-inmanage <roeec@restartit.co.il>
2022-01-10 11:58:10 +0200

    implement react hook form

--- clear_me/src/containers/memberForm/MemberForm.js
+++ clear_me/src/containers/memberForm/MemberForm.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useNavigate, useParams } from 'react-router-dom';
 import styled, { useTheme } from 'styled-components';
 import AppButton from '../../components/appButton/AppButton';
@@ -36,20 +37,23 @@ export default function MemberForm() {
 
   const { memberId } = useParams();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { onChange, ...rest } = register('organizationId', { required: true });
+
   const [member, setMember] = useState(null);
-  const [inputValue, setInputValue] = useState('');
-  const [showError, setShowError] = useState(false);
 
-  const onSubmit = () => {
-    if (inputValue) {
-      setShowError(false);
-
+  const onSubmit = (data) => {
+    if (data?.organizationId) {
       const { showToast } = appContext?.appState;
-      if (inputValue === member?.organization_id) {
+      if (data?.organizationId === member?.organization_id) {
         return typeof showToast === 'function' && showToast('The value is the same initial value..');
       }
 
-      return apiService.updateMember(member?.id, { organization_id: inputValue }).then((res) => {
+      return apiService.updateMember(member?.id, { organization_id: data?.organizationId }).then((res) => {
         if (res) {
           const { getOrganizationsAndMembers } = appContext?.appState?.api;
 
@@ -61,19 +65,10 @@ export default function MemberForm() {
         }
       });
     }
-
-    return setShowError(true);
   };
 
   const onInputValueChange = (e) => {
-    const { value } = e.target;
-    if (!value) {
-      setShowError(true);
-    } else {
-      setShowError(false);
-    }
-
-    return setInputValue(value);
+    onChange(e);
   };
 
   useEffect(() => {
@@ -81,10 +76,8 @@ export default function MemberForm() {
       apiService.getMemeberDetails(Number(memberId)).then((res) => {
         if (res) {
           setMember(res);
-
-          setInputValue(res?.organization_id);
         }
-      });      
+      });
     }
 
     return () => {};
@@ -96,19 +89,19 @@ export default function MemberForm() {
 
       <h3>{`Member form ${member?.name ? `- ${member?.name}` : ''}`}</h3>
 
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Label>Organization id</Label>
 
-        <Input type="text" placeholder="Organization id" value={inputValue} onChange={onInputValueChange} />
+        <Input {...rest} defaultValue={member?.organization_id} onChange={onInputValueChange} />
 
-        <AppButton onButtonClick={() => onSubmit()} buttonText={'Submit update'} />
+        <ErrorText
+          isParagragh={true}
+          text={`${errors?.organizationId ? "Value can't be empty" : '\0'}`}
+          style={theme.fontSizes.small}
+        />
+
+        <AppButton onButtonClick={handleSubmit(onSubmit)} buttonText={'Submit update'} />
       </Form>
-
-      <ErrorText
-        isParagragh={true}
-        text={`${!!showError ? "Value can't be empty" : ''}`}
-        style={theme.fontSizes.small}
-      />
     </PageWrapper>
   );
 }
